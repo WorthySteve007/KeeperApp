6f745a15cfb76a787b843f1e6d64730c9e29d5ca
Risav Katara <87906280+WorthySteve007@users.noreply.github.com>
2022-07-18 03:04:59 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  return (
+    <footer>
+      <p>Copyright © {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
